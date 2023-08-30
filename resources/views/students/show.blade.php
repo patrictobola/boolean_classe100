ba98be525dcadbd273c876c009b33405a66380b1
Mario-Marchio <mariomarchio1610@gmail.com>
2023-08-30 12:53:45 +0200

    added buttons to show

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -1,15 +1,27 @@
 @extends('layouts.main')
 
 @section('main')
-    <div class="container">
+    <div class="container mt-5">
         <div class="row">
-            <div class="col-md-6">
-                <a href="{{ route('students.edit', $student) }}" class="btn btn-primary">edit student</a>
-            </div>
             <p><strong>first name:</strong> {{ $student->first_name }}</p>
             <p><strong>last name:</strong> {{ $student->last_name }}</p>
             <p><strong>birth date:</strong> {{ $student->birth }}</p>
             <p><strong>city:</strong> {{ $student->city }}</p>
+            <div class="container d-flex justify-content-between">
+                <form action="{{ route('students.destroy', $student) }}" method="Post" class="delete-form ms-2"
+                    data-name="{{ $student->id }}">
+                    @method('delete')
+                    @csrf
+                    <button type="submit" class="btn btn-danger">delete</button>
+                </form>
+                <a class="btn btn-primary" href="{{ route('home') }}">Torna alla Home</a>
+                <div class="col-md-6 ">
+                    <a href="{{ route('students.edit', $student) }}" class="btn btn-primary">edit student</a>
+                </div>
+            </div>
         </div>
     </div>
+@endsection
+@section('scripts')
+    @vite('resources/js/form-delete.js')
 @endsection
