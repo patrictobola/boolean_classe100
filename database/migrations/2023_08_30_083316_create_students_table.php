33e4284d94f8337eb5bf935a9e50a77415ed5050
patrictobola <tobola92@gmail.com>
2023-08-30 10:33:29 +0200

    done student -cmsr

--- database/migrations/2023_08_30_083316_create_students_table.php
+++ database/migrations/2023_08_30_083316_create_students_table.php
@@ -13,8 +13,6 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('surname');
             $table->timestamps();
         });
     }
