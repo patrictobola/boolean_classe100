3db719b5ab99ad3fcaef5a051b0cf2ab2a643f38
Andrea Creazzi <andreacreazzi@libero.it>
2023-08-30 12:42:56 +0200

    add validation on create

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -6,6 +6,17 @@
 
 @section('main')
     <div class="container mt-5">
+        <div class="card">
+            @if ($errors->any())
+                <div class="alert alert-danger mb-0">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </div>
         <form action="{{ route('students.store') }}" method="POST">
             @csrf
             <div class="row">
@@ -13,24 +24,38 @@
                 <div class="col-6">
 
                     <label for="first_name" class="form-label">Nome:</label>
-                    <input type="text" class="form-control" id="first_name" name="first_name" placeholder="Inserisci nome">
+                    <input type="text" class="form-control @error('first_name') is-invalid @enderror" id="first_name"
+                        name="first_name" placeholder="Inserisci nome">
+                    @error('first_name')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="col-6">
 
                     <label for="last_name" class="form-label">Cognome:</label>
-                    <input type="text" class="form-control" id="last_name" name="last_name"
-                        placeholder="Inserisci cognome">
+                    <input type="text" class="form-control @error('last_name') is-invalid @enderror" id="last_name"
+                        name="last_name" placeholder="Inserisci cognome">
+                    @error('last_name')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="col-6">
 
-                    <label for="city" class="form-label">Città</label>
-                    <input type="text" class="form-control" id="city" name="city" placeholder="Inserisci città">
+                    <label for="city" class="form-label">Città:</label>
+                    <input type="text" class="form-control @error('city') is-invalid @enderror" id="city"
+                        name="city" placeholder="Inserisci città">
+                    @error('city')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="col-6">
 
-                    <label for="birthday" class="form-label">Email address</label>
+                    <label for="birthday" class="form-label @error('birth') is-invalid @enderror">Data di nascita:</label>
                     <input type="date" class="form-control" id="birthday" name="birth"
                         placeholder="Inserisci data di nascita">
+                    @error('birth')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
                 </div>
             </div>
             <button class="btn btn-success m-3">Invia</button>
