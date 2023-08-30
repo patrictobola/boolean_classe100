e91f8b7a5b6aa668875f92ef569a2a0a021199a0
Mario-Marchio <mariomarchio1610@gmail.com>
2023-08-30 16:40:26 +0200

    css added to show

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -3,25 +3,43 @@
 @section('main')
     <div class="container mt-5">
         <div class="row">
-            <p><strong>first name:</strong> {{ $student->first_name }}</p>
-            <p><strong>last name:</strong> {{ $student->last_name }}</p>
-            <p><strong>birth date:</strong> {{ $student->birth }}</p>
-            <p><strong>city:</strong> {{ $student->city }}</p>
-            <div class="container d-flex justify-content-between">
-                <form action="{{ route('students.destroy', $student) }}" method="Post" class="delete-form ms-2"
-                    data-name="{{ $student->id }}">
-                    @method('delete')
-                    @csrf
-                    <button type="submit" class="btn btn-danger">delete</button>
-                </form>
-                <a class="btn btn-primary" href="{{ route('home') }}">Torna alla Home</a>
-                <div class="col-md-6 ">
-                    <a href="{{ route('students.edit', $student) }}" class="btn btn-primary">edit student</a>
+            <div class="col-md-8 offset-md-2">
+                <table class="table table-bordered">
+                    <h1 class="text-center">Student page</h1>
+                    <tbody>
+                        <tr>
+                            <td><strong>First Name:</strong></td>
+                            <td>{{ $student->first_name }}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Last Name:</strong></td>
+                            <td>{{ $student->last_name }}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Birth Date:</strong></td>
+                            <td>{{ $student->birth }}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>City:</strong></td>
+                            <td>{{ $student->city }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="d-flex justify-content-between">
+                    <a class="btn btn-primary" href="{{ route('home') }}">Back to Home</a>
+                    <a href="{{ route('students.edit', $student) }}" class="btn btn-warning">Edit Student</a>
+                    <form action="{{ route('students.destroy', $student) }}" method="post" class="delete-form"
+                        data-name="{{ $student->id }}">
+                        @method('delete')
+                        @csrf
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
 @endsection
+
 @section('scripts')
     @vite('resources/js/form-delete.js')
 @endsection
